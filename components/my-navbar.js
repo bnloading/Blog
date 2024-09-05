47b57b239f484554b7fd38bd1ca387ab2531ec81
bnloading <nurbakhitjan5@gmail.com>
2024-09-05 23:29:34 +0500

    navbar oruulsan bn

--- components/my-navbar.js
+++ components/my-navbar.js
@@ -1,5 +1,6 @@
 import { Container, Row, Col, Navbar, Image, Card, Nav } from "react-bootstrap";
-export default () => {
+
+const MyNavbar = () => {
   return (
     <Navbar className="fj-navbar fj navbase" bg="transparent" expand="lg">
       <Navbar.Brand className="fj-navbar-brand">
@@ -7,12 +8,14 @@ export default () => {
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
+        <Nav className="ms-auto">
           <Nav.Link className="fj-navbar-item fj-navbar-link" href="/">
-            Негізгі бетn
+            Негізгі бет
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
   );
 };
+
+export default MyNavbar;
