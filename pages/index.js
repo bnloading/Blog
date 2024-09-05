db950198570a3481523a113fc925ce67d6a6c943
bnloading <nurbakhitjan5@gmail.com>
2024-09-05 21:51:24 +0500

    bootstrap holbloo

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,24 @@ import Head from "next/head";
 import Image from "next/image";
 import localFont from "next/font/local";
 import styles from "@/styles/Home.module.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
+import Alert from "react-bootstrap/Alert";
 export default function Home() {
-  return <>my blog</>;
+  return (
+    <div>
+      {[
+        "primary",
+        "secondary",
+        "success",
+        "danger",
+        "warning",
+        "info",
+        "light",
+        "dark",
+      ].map((variant) => (
+        <Alert key={variant} variant={variant}>
+          This is a {variant} alert—check it out!
+        </Alert>
+      ))}
+    </div>
+  );
 }
